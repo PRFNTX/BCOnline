1c9202f6fa06b005fba521a4205f7295d9d588e4
Brent Hughson <brent@Brents-MacBook-Pro.local>
2017-09-15 16:38:38 -0400

    early js

---
+++ Javascript/t1.js
@@ -0,0 +1,4 @@
+var test = document.querySelector("li a");
+var btn = document.querySelector("button");
+var bdy = document.querySelector("body");
+btn.addEventListener("click",function(){bdy.classList.toggle("one")});
